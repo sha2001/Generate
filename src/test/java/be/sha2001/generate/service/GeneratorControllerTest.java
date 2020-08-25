96180fae48223a3f3da6aa8529e1707fcae79c36
sha2001 <1etC5bl4i%rr5^qD>
2020-08-25 20:56:01 +0200

    test correction

--- src/test/java/be/sha2001/generate/service/GeneratorControllerTest.java
+++ src/test/java/be/sha2001/generate/service/GeneratorControllerTest.java
@@ -1,15 +1,14 @@
 package be.sha2001.generate.service;
 
-import be.sha2001.generate.GenerateApplication;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.jupiter.api.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.mock.web.MockServletContext;
-import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
-import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
@@ -17,16 +16,18 @@ import org.springframework.web.context.WebApplicationContext;
 import javax.servlet.ServletContext;
 
 @RunWith(SpringJUnit4ClassRunner.class)
-@ContextConfiguration(classes = {GenerateApplication.class})
-@WebAppConfiguration
+
+@SpringBootTest
+@AutoConfigureMockMvc
 class GeneratorControllerTest {
 
     @Autowired
     private WebApplicationContext wac;
+    @Autowired
     private MockMvc mockMvc;
 
     @Before
-    public void setup() throws Exception {
+    public void setup() {
         this.mockMvc = MockMvcBuilders.webAppContextSetup(this.wac).build();
     }
 
